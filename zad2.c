e3a91fd713bd0da4c1dc480c845f4099512536fc
dan-ch <61287095+dan-ch@users.noreply.github.com>
2020-11-15 20:02:08 +0100

    Add files via upload

---
+++ zad2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
+#include <math.h>
+
+double licz(int ile){
+    double wynik = 0;
+    for(int i=1; i< ile; i++){
+        wynik +=(pow(-1, i-1))/(2*i-1);
+    }
+    return wynik*4;
+}
+
+void forker(int ile){
+pid_t pid;
+
+if(ile > 0){
+    pid = fork();
+    if(pid < 0){
+        perror("fork");
+    }else if(pid == 0){
+        srand(time(NULL)^getpid());
+        int n = 100+rand()%4900;
+        printf("Dla n = %d wartość PI = %f\n", n, licz(n));
+    }
+    else{
+        forker(ile-1);
+    }
+    }
+}
+
+
+int main(){
+int P = 50;
+forker(P);
+
+return 0;
+}
+
